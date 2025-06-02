98a3d8fa332e135383ba09eaec88a3f0658650a3
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-06-02 16:52:04 -0300

    jogos e franquias a serem processadas

---
+++ games_to_scraper/games.py
@@ -0,0 +1,15 @@
+franchises = [
+    "Castlevania",
+    "Borderlands",
+    "Halo",
+    "Tomb Raider",
+    "The Witcher",
+    "Fallout",
+    "Mortal Kombat",
+    "Five Nights at Freddy's",
+]
+
+games = [
+    "Cyberpunk 2077",
+    "Until Dawn",
+]
